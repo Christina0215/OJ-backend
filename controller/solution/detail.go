1b57f0a7962967ae326629c15a5fadd4a471da80
Christina0215 <1532706870@qq.com>
2021-06-05 17:40:22 +0800

    fix bug

--- controller/solution/detail.go
+++ controller/solution/detail.go
@@ -11,9 +11,9 @@ func GetDetail(c *gin.Context) {
 
 	var solution model.Solution
 	db := orm.GetDB()
-	solutionId := c.Param("SolutionId")
+	solutionId := c.Param("solutionId")
 	if db.Where("ID = ?", solutionId).First(&solution).RecordNotFound() {
-		c.JSON(401, gin.H{"message": "该题目不存在哦"})
+		c.JSON(402, gin.H{"message": "该题目不存在哦"})
 		return
 	}
 	c.JSON(200, solution.GetData("detail"))
